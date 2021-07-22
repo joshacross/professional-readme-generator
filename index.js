1a2c174d57fa412c0532964b45bf653cd825808d
Killa J <joshuaalexandercross@gmail.com>
2021-07-22 14:22:33 -0500

    finalized index.js and page-tempate files

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const generateSite = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
-const { writeFile, copyFile } = require('./utils/generate-site.js');
+const { writeFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template');
 
 const promptUser = () => {
     return inquirer.prompt([
       {
         type: 'input',
-        name: 'name',
+        name: 'projectTitle',
         message: 'What is the Project Title of your application? (Required)',
-        validate: nameInput => {
-          if (nameInput) {
+        validate: projectTitleInput => {
+          if (projectTitleInput) {
             return true;
           } else {
             console.log('Please enter a title for your project...');
@@ -20,10 +20,10 @@ const promptUser = () => {
       },
       {
         type: 'input',
-        name: 'github',
+        name: 'projectDescription',
         message: 'Enter a short description of your project. Remember, to explain the what, why and how, motivation, and why you built this project. (Required)',
-        validate: githubInput => {
-          if (githubInput) {
+        validate: projectDescriptionInput => {
+          if (projectDescriptionInput) {
             return true;
           } else {
             console.log('Description field cannot be blank. Please try again.');
@@ -33,36 +33,68 @@ const promptUser = () => {
       },
       {
         type: 'input',
-        name: 'github',
+        name: 'problemSolution',
         message: 'What problem does your application solve? (Required)',
-        validate: githubInput => {
-          if (githubInput) {
+        validate: problemSolutionInput => {
+          if (problemSolutionInput) {
             return true;
           } else {
-            console.log('Description field cannot be blank. Please try again.');
+            console.log('Problem/Solution field cannot be blank. Please try again.');
             return false;
           }
         }
       },
       {
         type: 'input',
-        name: 'github',
+        name: 'learning',
         message: 'What did you learn about in this project? (Required)',
-        validate: githubInput => {
-          if (githubInput) {
+        validate: learningInput => {
+          if (learningInput) {
             return true;
           } else {
-            console.log('Description field cannot be blank. Please try again.');
+            console.log('Learning field cannot be blank. Please try again.');
             return false;
           }
         }
       },
       {
         type: 'input',
-        name: 'github',
+        name: 'functionality',
         message: 'What makes your project stand out? (Required)',
-        validate: githubInput => {
-          if (githubInput) {
+        validate: functionalityInput => {
+          if (funtionalityInput) {
+            return true;
+          } else {
+            console.log('Functionality field cannot be blank. Please try again.');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node', 'Express', 'React', 'GraphQL', 'Apollo', 'MongoDB', 'Mongoose', 'mySQL', 'Sequelize', 'template-engine' ]
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide instructions on how to install your project. (Required)',
+        validate: installationInput => {
+          if (installationInput) {
+            return true;
+          } else {
+            console.log('Installation field cannot be blank. Please try again.');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'functionality',
+        message: 'What makes your project stand out? (Required)',
+        validate: functionalityInput => {
+          if (funtionalityInput) {
             return true;
           } else {
             console.log('Description field cannot be blank. Please try again.');
@@ -79,13 +111,91 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'contributing',
-        message: 'How may others contribute?',
+        message: 'How may others contribute to your project?',
       },
       {
         type: 'input',
-        name: 'about',
-        message: 'Provide some information about yourself:',
-        when: ({ confirmAbout }) => confirmAbout
+        name: 'tests',
+        message: 'What tests have you performed on this project? (Required)',
+        validate: testInput => {
+          if (testInput) {
+            return true;
+          } else {
+            console.log('Test field cannot be blank. Please try again.');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'firstName',
+        message: 'What is your first name? (Required)',
+        validate: firstNameInput => {
+          if (firstNameInput) {
+            return true;
+          } else {
+            console.log('Please provide your first name.');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'lastName',
+        message: 'What is your last name? (Required)',
+        validate: lastNameInput => {
+          if (lastNameInput) {
+            return true;
+          } else {
+            console.log('Please provide your last name.');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email? (Required)',
+        validate: lastNameInput => {
+          if (lastNameInput) {
+            return true;
+          } else {
+            console.log('Please provide your last name.');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'input',
+        name: 'gitHubUsername',
+        message: 'What is your Github Username? (Required)',
+        validate: gitHubUsernameInput => {
+          if (gitHubUsernameInput) {
+            return true;
+          } else {
+            console.log('Github Username cannot be blank.');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'checkbox',
+        name: 'dataLicense',
+        message: 'What license is your project under? (Required)',
+        choices: ['MIT', 'GNU AGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'Boost Software License 1.0', 'The Unlicense', 'other']
+      },
+      {
+        type: 'input',
+        name: 'licenseLink',
+        message: 'What is the link to this license? (Required)',
+        validate: licenseLinkInput => {
+          if (lincenseLinkInput) {
+            return true;
+          } else {
+            console.log('License Link cannot be blank.');
+            return false;
+          }
+        }
       }
     ]);
   };
